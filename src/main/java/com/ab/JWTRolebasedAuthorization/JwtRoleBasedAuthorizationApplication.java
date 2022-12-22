dc4c457237cc47787b44d6518435f6d4851075e8
abhishekrtpl <abhishek.sharma@ranosys.com>
2022-12-22 13:36:42 +0530

    project created

---
+++ src/main/java/com/ab/JWTRolebasedAuthorization/JwtRoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.ab.JWTRolebasedAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRoleBasedAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRoleBasedAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ab/JWTRolebasedAuthorization/JwtRoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.JWTRolebasedAuthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRoleBasedAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
